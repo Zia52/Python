113bd8866e5fd47d947bd227ce4ec30ab53d99e1
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-10 21:39:47 +0600

    learn concatenation

---
+++ Variables & Data Type/Working With String.py
@@ -0,0 +1,15 @@
+#String are basically just plan text. Any text that we want to have inside of our program we can store inside string.
+
+#Example1
+print("Example 1")
+print("Python Is Awosem \n most well known programming languages") #(/n):insert a new line into the string
+
+#Example2
+print("Example 2")
+pharse = "Learn Python" #(Variable_name-pharse) & (type-String)
+print(pharse)
+
+#Example3 
+print("Concatenation")
+phrase = "Learn python"
+print(phrase + "is cool") #+ can add another string. Basically appending another string onto another one, they call that Concatenation.
