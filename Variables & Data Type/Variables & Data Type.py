453443d1ca3ac4f4a6081ee20ed1fb0966e79f0b
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-10 21:17:58 +0600

    change varible

--- Variables & Data Type/Variables & Data Type.py
+++ Variables & Data Type/Variables & Data Type.py
@@ -1,7 +1,8 @@
-character_name = "Xipa"
-character_age = "24"
-
+character_name = "Xipa" #Variable_name, Type: int,str
+character_age = "24"    
 print("There once was a man name " + character_name + ", ")
 print("he was " + character_age + " years old.")
+
+character_name = "Zia"
 print("He really liked the name " + character_name + " ")
 print("but didn't like being " + character_age + ".")
