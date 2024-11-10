ad545e93351a1f3fa2a30775239b308a30987f2b
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-10 21:10:22 +0600

    print age and name

--- Variables & Data Type/Variables & Data Type.py
+++ Variables & Data Type/Variables & Data Type.py
@@ -1,4 +1,7 @@
 character_name = "Xipa"
 character_age = "24"
 
-print("There once was a man name" + character_name + ", ")
+print("There once was a man name " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + " ")
+print("but didn't like being " + character_age + ".")
