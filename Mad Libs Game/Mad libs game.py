d9d4d42e93db0ac3bb34eeae2da3decf7babef29
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-12 19:06:21 +0600

    madlibs game

---
+++ Mad Libs Game/Mad libs game.py
@@ -0,0 +1,8 @@
+#mad libs game where you enter a bunch of random words. like verbs, noun, names, color take all the words and put them into a story randomly.
+
+color = input("Enter a color name: ") 
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity name: ")
+print("Rose are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
