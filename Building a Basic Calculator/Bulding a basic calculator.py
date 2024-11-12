e91d95d5ee19dd26a56fdbdfc7105919fdb7e8d3
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-12 18:46:36 +0600

    build calculator

---
+++ Building a Basic Calculator/Bulding a basic calculator.py
@@ -0,0 +1,8 @@
+#Build a basic calculator using python. 
+#Where we we will get two numbers from a user, and than will add those number together and print the answer out onto the screen.
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
+
+result = float(num1) + float(num2)
+
+print (result)
