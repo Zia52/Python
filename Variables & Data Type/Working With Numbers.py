eacdf2035e5ae706c0007741cfc20e969c4257d0
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-12 17:27:21 +0600

    numbers

---
+++ Variables & Data Type/Working With Numbers.py
@@ -0,0 +1,27 @@
+my_num = 10
+print(str(my_num) + " lionel messi") #(str) use print string with number
+
+#absoulate value 
+my_num = -10
+print(abs(my_num))
+
+#power value
+print(pow(5, 7)) #5^7
+
+#max, min
+print(max(4, 6))
+print(min(4, 6))
+
+#round the number 
+print(round(9.8)) #9.8 round it in 10
+
+#importing: import external code in your file. Access these specific math functions called mython math.
+#floor just grab the lowest number
+#ceil
+#sqrt
+print("math import function")
+from math import *
+print(floor(30))
+print(ceil(40))
+print(sqrt(40))
+
