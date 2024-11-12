aa926e6e2af3a7790f82a07c9c97613119c35440
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-12 18:24:40 +0600

    Input Function

---
+++ Getting Import from User/User Input.py
@@ -0,0 +1,9 @@
+#Input function: allow user to input information in our program.
+
+name = input("Enter your name: ")#input function use to get information from user
+print("Hello " + name + "!") #input(): Example, user give a name call David, it was store in python variable called name, #print(): than it going out to print out Hello David.
+
+#Ex2
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! your are " +age) #user give his name and age, then variable called "name" it store the user given value, than print function print user name and age. Ex: Hello Pinog! your age is 24
