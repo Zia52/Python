5f7c83578bebda1febbdc3a17975b3c34f0ed8e5
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-12 19:36:20 +0600

    lists

---
+++ Lists/list.py
@@ -0,0 +1,23 @@
+#list store the information and it allow us to organize them and keep track them a lot of easier.
+
+#Example1
+friends = ["David", "Mike", "Pong"] #friends list store all the values
+print(friends)
+
+#Example2
+friends = ["David", "Mike", "Pong"] #friends list store all the values
+print(friends[0]) #indexing: only print David 
+
+#Example3
+friends = ["David", "Mike", "Pong"] #friends list store all the values
+print(friends[1:]) #print all the elemnt after 1
+
+#Example4
+Batch_42 = ["Shaown", "Riyaz", "Zaman", "Raihan", "Sadik" "Pial"]
+print(Batch_42[1:4]) #print only 1 to 4 indexing value (name)
+
+#Example5
+Batch_42 = ["Shaown", "Riyaz", "Zaman", "Raihan", "Sadik" "Pial"]
+Batch_42[1] = "Tashrif" #changeing the name of index1 than update and print
+print(Batch_42) 
+
